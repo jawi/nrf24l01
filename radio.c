f596032f7dc4c5e659c7e01960fc8b09b2de7307
neilrqm <neilrqm@51016f6c-9368-11de-9a30-078c9dd406eb>
2009-08-29 00:34:05 +0000

    - Changed CSN pin to PE6 and interrupt to INT7 so that they don't conflict with the joystick. - Removed leading underscores from private functions to match RTOS style. - Changed all private members to static to enforce access privileges. - Changed radio initialization delay to 11 ms before the registers are configured and 2 ms after the registers are configured.

--- radio.c
+++ radio.c
@@ -12,43 +12,45 @@
 #define ADDRESS_LENGTH 5
 
 // Pin definitions for chip select and chip enable on the radio module
-#define RADIO_DDR	DDRB
-#define RADIO_PORT	PORTB
-#define RADIO_CE	PORTB4
-#define RADIO_CSN	PORTB5
+#define CE_DDR		DDRB
+#define CSN_DDR		DDRE
+#define CE_PORT		PORTB
+#define CSN_PORT	PORTE
+#define CE_PIN		PB4
+#define CSN_PIN		PE6
 
 // Definitions for selecting and enabling the radio
-#define RADIO_CSN_HIGH()	RADIO_PORT |=  (1<<RADIO_CSN);
-#define RADIO_CSN_LOW()		RADIO_PORT &= ~(1<<RADIO_CSN);
-#define RADIO_CE_HIGH()		RADIO_PORT |=  (1<<RADIO_CE);
-#define RADIO_CE_LOW()      RADIO_PORT &= ~(1<<RADIO_CE);
+#define CSN_HIGH()	CSN_PORT |=  _BV(CSN_PIN);
+#define CSN_LOW()	CSN_PORT &= ~_BV(CSN_PIN);
+#define CE_HIGH()	CE_PORT |=  _BV(CE_PIN);
+#define CE_LOW()	CE_PORT &= ~_BV(CE_PIN);
 
 // Flag which denotes that the radio is currently transmitting
-volatile uint8_t transmit_lock;
+static volatile uint8_t transmit_lock;
 // tracks the payload widths of the Rx pipes
-volatile uint8_t rx_pipe_widths[6] = {32, 32, 0, 0, 0, 0};
+static volatile uint8_t rx_pipe_widths[6] = {32, 32, 0, 0, 0, 0};
 // holds the transmit address (Rx pipe 0 is set to this address when transmitting with auto-ack enabled).
-volatile uint8_t tx_address[5] = { 0xe7, 0xe7, 0xe7, 0xe7, 0xe7 };
+static volatile uint8_t tx_address[5] = { 0xe7, 0xe7, 0xe7, 0xe7, 0xe7 };
 // holds the receiver address for Rx pipe 0 (the address is overwritten when transmitting with auto-ack enabled).
-volatile uint8_t rx_pipe0_address[5] = { 0xe7, 0xe7, 0xe7, 0xe7, 0xe7 };
+static volatile uint8_t rx_pipe0_address[5] = { 0xe7, 0xe7, 0xe7, 0xe7, 0xe7 };
 // the driver keeps track of the success status for the last 16 transmissions
-volatile uint16_t tx_history = 0xFF;
+static volatile uint16_t tx_history = 0xFF;
 
-volatile RADIO_TX_STATUS tx_last_status = RADIO_TX_SUCCESS;
+static volatile RADIO_TX_STATUS tx_last_status = RADIO_TX_SUCCESS;
 
 extern void radio_rxhandler(uint8_t pipenumber);
 
 /**
  * Retrieve the status register.
  */
-uint8_t _get_status()
+static uint8_t get_status()
 {
 	uint8_t status = 0;
-	RADIO_CSN_LOW();
+	CSN_LOW();
 
 	status = SPI_Write_Byte(NOP);
 
-	RADIO_CSN_HIGH();
+	CSN_HIGH();
 
 	return status;
 }
@@ -58,15 +60,15 @@ uint8_t _get_status()
  * \param value The value to write to the given register (the whole register is overwritten).
  * \return The status register.
  */
-uint8_t _set_register(radio_register_t reg, uint8_t* value, uint8_t len)
+static uint8_t set_register(radio_register_t reg, uint8_t* value, uint8_t len)
 {
 	uint8_t status;
-	RADIO_CSN_LOW();
+	CSN_LOW();
 
 	status = SPI_Write_Byte(W_REGISTER | (REGISTER_MASK & reg));
 	SPI_Write_Block(value, len);
 
-	RADIO_CSN_HIGH();
+	CSN_HIGH();
 
 	return status;
 }
@@ -78,7 +80,7 @@ uint8_t _set_register(radio_register_t reg, uint8_t* value, uint8_t len)
  * 		register contents, then the remaining bytes will be overwritten with 0xFF.
  * \param len The length of the buffer.
  */
-uint8_t _get_register(radio_register_t reg, uint8_t* buffer, uint8_t len)
+static uint8_t get_register(radio_register_t reg, uint8_t* buffer, uint8_t len)
 {
 	uint8_t status, i;
 	for (i = 0; i < len; i++)
@@ -87,12 +89,12 @@ uint8_t _get_register(radio_register_t reg, uint8_t* buffer, uint8_t len)
 		// To remove the risk, we set the buffer elements to NOP instructions.
 		buffer[i] = 0xFF;
 	}
-	RADIO_CSN_LOW();
+	CSN_LOW();
 
 	status = SPI_Write_Byte(R_REGISTER | (REGISTER_MASK & reg));
 	SPI_ReadWrite_Block(NULL, buffer, len);
 
-	RADIO_CSN_HIGH();
+	CSN_HIGH();
 
 	return status;
 }
@@ -104,9 +106,9 @@ uint8_t _get_register(radio_register_t reg, uint8_t* buffer, uint8_t len)
  * \param buffer An array for the instruction's return data.  This can be NULL if the instruction has no output.
  * \param len The length of the data and buffer arrays.
  */
-void send_instruction(uint8_t instruction, uint8_t* data, uint8_t* buffer, uint8_t len)
+static void send_instruction(uint8_t instruction, uint8_t* data, uint8_t* buffer, uint8_t len)
 {
-    RADIO_CSN_LOW();
+    CSN_LOW();
 	// send the instruction
 	SPI_Write_Byte(instruction);
 	// pass in args
@@ -118,20 +120,20 @@ void send_instruction(uint8_t instruction, uint8_t* data, uint8_t* buffer, uint8
 			SPI_ReadWrite_Block(data, buffer, len);
 	}
     // resynch SPI
-    RADIO_CSN_HIGH();
+    CSN_HIGH();
 }
 
 /**
  * Switch the radio to receive mode.  If the radio is already in receive mode, this does nothing.
  */
-void _set_rx_mode()
+static void set_rx_mode()
 {
 	uint8_t config;
-	_get_register(CONFIG, &config, 1);
+	get_register(CONFIG, &config, 1);
 	if ((config & _BV(PRIM_RX)) == 0)
 	{
 		config |= _BV(PRIM_RX);
-		_set_register(CONFIG, &config, 1);
+		set_register(CONFIG, &config, 1);
 		// the radio takes 130 us to power up the receiver.
 		_delay_us(65);
 		_delay_us(65);
@@ -141,14 +143,14 @@ void _set_rx_mode()
 /**
  * Switch the radio to transmit mode.  If the radio is already in transmit mode, this does nothing.
  */
-void _set_tx_mode()
+static void set_tx_mode()
 {
 	uint8_t config;
-	_get_register(CONFIG, &config, 1);
+	get_register(CONFIG, &config, 1);
 	if ((config & _BV(PRIM_RX)) != 0)
 	{
 		config &= ~_BV(PRIM_RX);
-		_set_register(CONFIG, &config, 1);
+		set_register(CONFIG, &config, 1);
 		// The radio takes 130 us to power up the transmitter
 		// You can delete this if you're sending large packets (I'm thinking > 25 bytes, but I'm not sure) because it
 		// sending the bytes over SPI can take this long.
@@ -162,12 +164,12 @@ void _set_tx_mode()
  * the pipe 0 address is set to the transmit address while the radio is transmitting (this is how the radio receives
  * auto-ack packets).
  */
-void _reset_pipe0_address()
+static void reset_pipe0_address()
 {
 	if (rx_pipe_widths[RADIO_PIPE_0] != 0)
 	{
 		// reset the pipe 0 address if pipe 0 is enabled.
-		_set_register(RX_ADDR_P0, (uint8_t*)rx_pipe0_address, ADDRESS_LENGTH);
+		set_register(RX_ADDR_P0, (uint8_t*)rx_pipe0_address, ADDRESS_LENGTH);
 	}
 }
 
@@ -176,7 +178,7 @@ void _reset_pipe0_address()
  * This configures the radio to its max-power, max-packet-header-length settings.  If you want to reduce power consumption
  * or increase on-air payload bandwidth, you'll have to change the config.
  */
-void _config()
+static void configure_registers()
 {
 	uint8_t value;
 
@@ -184,28 +186,28 @@ void _config()
 
 	// set address width to 5 bytes.
 	value = ADDRESS_LENGTH - 2;			// 0b11 for 5 bytes, 0b10 for 4 bytes, 0b01 for 3 bytes
-	_set_register(SETUP_AW, &value, 1);
+	set_register(SETUP_AW, &value, 1);
 
 	// set Enhanced Shockburst retry to every 586 us, up to 5 times.  If packet collisions are a problem even with AA enabled,
 	// then consider changing the retry delay to be different on the different stations so that they do not keep colliding on each retry.
 	value = 0x15;
 	//value = 0x10;
-	_set_register(SETUP_RETR, &value, 1);
+	set_register(SETUP_RETR, &value, 1);
 
 	// Set to use 2.4 GHz channel 110.
 	value = CHANNEL;
-	_set_register(RF_CH, &value, 1);
+	set_register(RF_CH, &value, 1);
 
 	// Set radio to 2 Mbps and high power.  Leave LNA_HCURR at its default.
 	value = _BV(RF_DR) | _BV(LNA_HCURR);
-	_set_register(RF_SETUP, &value, 1);
+	set_register(RF_SETUP, &value, 1);
 
 	// Enable 2-byte CRC and power up in receive mode.
 	value = _BV(EN_CRC) | _BV(CRCO) | _BV(PWR_UP) | _BV(PRIM_RX);
-	_set_register(CONFIG, &value, 1);
+	set_register(CONFIG, &value, 1);
 
     value = _BV(RX_DR) | _BV(TX_DS) | _BV(MAX_RT);
-    _set_register(STATUS, &value, 1);    
+    set_register(STATUS, &value, 1);
 
 	send_instruction(FLUSH_TX, NULL, NULL, 0);
 	send_instruction(FLUSH_RX, NULL, NULL, 0);
@@ -216,25 +218,31 @@ void Radio_Init()
 	transmit_lock = 0;
 
 	// disable radio during config
-	RADIO_CE_LOW();
+	CE_LOW();
 
 	// set as output AT90 pins connected to the radio's slave select and chip enable pins.
-	RADIO_DDR |= _BV(RADIO_CSN) | _BV(RADIO_CE);
+	CE_DDR |= _BV(CE_PIN);
+	CSN_DDR |= _BV(CSN_PIN);
 
 	// Enable radio interrupt.  This interrupt is triggered when data are received and when a transmission completes.
-	DDRE &= ~_BV(PORTE4);
-	EICRB |= _BV(ISC41);
-	EICRB &= ~_BV(ISC40);
-	EIMSK |= _BV(INT4);
+	DDRE &= ~_BV(PORTE7);
+	EICRB |= _BV(ISC71);
+	EICRB &= ~_BV(ISC70);
+	EIMSK |= _BV(INT7);
+
+	// A 10.3 ms delay is required between power off and power on states (controlled by 3.3 V supply)
+	// I suspect this isn't necessary as long as nothing is sent or received for 10 ms, but what the heck,
+	// we can afford to waste some time during initialization.
+	_delay_ms(11);
 
 	// Configure the radio registers that are not application-dependent.
-	_config();
+	configure_registers();
 
-	// Wait for the radio to power up.
-	_delay_ms(10);
+	// A 1.5 ms delay is required between power down and power up states (controlled by PWR_UP bit in CONFIG)
+	_delay_ms(2);
 
 	// enable radio as a receiver
-	RADIO_CE_HIGH();
+	CE_HIGH();
 }
 
 // default address for pipe 0 is 0xe7e7e7e7e7
@@ -262,28 +270,28 @@ void Radio_Configure_Rx(RADIO_PIPE pipe, uint8_t* address, uint8_t enable)
 
 	// Set the address.  We set this stuff even if the pipe is being disabled, because for example the transmitter
 	// needs pipe 0 to have the same address as the Tx address for auto-ack to work, even if pipe 0 is disabled.
-	_set_register(RX_ADDR_P0 + pipe, address, pipe > RADIO_PIPE_1 ? 1 : ADDRESS_LENGTH);
+	set_register(RX_ADDR_P0 + pipe, address, pipe > RADIO_PIPE_1 ? 1 : ADDRESS_LENGTH);
 
 	// Set auto-ack.
-	_get_register(EN_AA, &value, 1);
+	get_register(EN_AA, &value, 1);
 	if (use_aa)
 		value |= _BV(pipe);
 	else
 		value &= ~_BV(pipe);
-	_set_register(EN_AA, &value, 1);
+	set_register(EN_AA, &value, 1);
 
 	// Set the pipe's payload width.  If the pipe is being disabled, then the payload width is set to 0.
 	value = enable ? payload_width : 0;
-	_set_register(RX_PW_P0 + pipe, &value, 1);
+	set_register(RX_PW_P0 + pipe, &value, 1);
 	rx_pipe_widths[pipe] = value;
 
 	// Enable or disable the pipe.
-	_get_register(EN_RXADDR, &value, 1);
+	get_register(EN_RXADDR, &value, 1);
 	if (enable)
 		value |= _BV(pipe);
 	else
 		value &= ~_BV(pipe);
-	_set_register(EN_RXADDR, &value, 1);
+	set_register(EN_RXADDR, &value, 1);
 
 }
 
@@ -295,7 +303,7 @@ void Radio_Set_Tx_Addr(uint8_t* address)
 	tx_address[2] = address[2];
 	tx_address[3] = address[3];
 	tx_address[4] = address[4];
-	_set_register(TX_ADDR, address, ADDRESS_LENGTH);
+	set_register(TX_ADDR, address, ADDRESS_LENGTH);
 }
 
 void Radio_Configure(RADIO_DATA_RATE dr, RADIO_TX_POWER power)
@@ -308,7 +316,7 @@ void Radio_Configure(RADIO_DATA_RATE dr, RADIO_TX_POWER power)
 	//Radio_Set_Tx_Addr(address);
 
 	// set the data rate and power bits in the RF_SETUP register
-	_get_register(RF_SETUP, &value, 1);
+	get_register(RF_SETUP, &value, 1);
 
 	value |= 3 << RF_PWR;			// set the power bits so that the & will mask the power value in properly.
 	value &= power << RF_PWR;		// mask the power value into the RF status byte.
@@ -318,7 +326,7 @@ void Radio_Configure(RADIO_DATA_RATE dr, RADIO_TX_POWER power)
 	else
 		value &= ~_BV(RF_DR);
 
-	_set_register(RF_SETUP, &value, 1);
+	set_register(RF_SETUP, &value, 1);
 }
 
 uint8_t Radio_Transmit(radiopacket_t* payload, RADIO_TX_WAIT wait)
@@ -331,25 +339,25 @@ uint8_t Radio_Transmit(radiopacket_t* payload, RADIO_TX_WAIT wait)
     transmit_lock = 1;
 
 	// disable the radio while writing to the Tx FIFO.
-    RADIO_CE_LOW();
+    CE_LOW();
 
-	_set_tx_mode();
+	set_tx_mode();
 
     // for auto-ack to work, the pipe0 address must be set to the Tx address while the radio is transmitting.
     // The register will be set back to the original pipe 0 address when the TX_DS or MAX_RT interrupt is asserted.
-    _set_register(RX_ADDR_P0, (uint8_t*)tx_address, ADDRESS_LENGTH);
+    set_register(RX_ADDR_P0, (uint8_t*)tx_address, ADDRESS_LENGTH);
 
     // enable SPI
-    RADIO_CSN_LOW();
+    CSN_LOW();
     // send the "write transmit payload" instruction.
     SPI_Write_Byte(W_TX_PAYLOAD);
     // write the payload to the Tx FIFO
     SPI_Write_Block((uint8_t*)payload,len);
     // disable SPI
-    RADIO_CSN_HIGH();
+    CSN_HIGH();
 
     // start the transmission.
-    RADIO_CE_HIGH();
+    CE_HIGH();
 
     if (wait == RADIO_WAIT_FOR_TX)
     {
@@ -380,9 +388,9 @@ RADIO_RX_STATUS Radio_Receive(radiopacket_t* buffer)
 
 	transmit_lock = 0;
 
-	RADIO_CE_LOW();
+	CE_LOW();
 
-    status = _get_status();
+    status = get_status();
 	pipe_number =  (status & 0xE) >> 1;
 
 	if (pipe_number == RADIO_PIPE_EMPTY)
@@ -403,7 +411,7 @@ RADIO_RX_STATUS Radio_Receive(radiopacket_t* buffer)
 		// Move the data payload into the local
 		send_instruction(R_RX_PAYLOAD, (uint8_t*)buffer, (uint8_t*)buffer, rx_pipe_widths[pipe_number]);
 
-		status = _get_status();
+		status = get_status();
 		pipe_number =  (status & 0xE) >> 1;
 
 		if (pipe_number != RADIO_PIPE_EMPTY)
@@ -412,7 +420,7 @@ RADIO_RX_STATUS Radio_Receive(radiopacket_t* buffer)
 			result = RADIO_RX_SUCCESS;
 	}
 
-	RADIO_CE_HIGH();
+	CE_HIGH();
 
 	transmit_lock = 0;
 
@@ -437,14 +445,14 @@ uint8_t Radio_Drop_Rate()
 }
 
 // Interrupt handler
-ISR(INT4_vect)
+ISR(INT7_vect)
 {
     uint8_t status;
     uint8_t pipe_number;
-    
-    RADIO_CE_LOW();
 
-    status = _get_status();
+    CE_LOW();
+
+    status = get_status();
 
     if (status & _BV(RX_DR))
     {
@@ -456,8 +464,8 @@ ISR(INT4_vect)
     {
         // if there's nothing left to transmit, switch back to receive mode.
         transmit_lock = 0;
-        _reset_pipe0_address();
-        _set_rx_mode();
+        reset_pipe0_address();
+        set_rx_mode();
 
     	// indicate in the history that a packet was transmitted successfully by appending a 1.
     	tx_history <<= 1;
@@ -468,15 +476,15 @@ ISR(INT4_vect)
     else if (status & _BV(MAX_RT))
     {
         // enable SPI
-        RADIO_CSN_LOW();
+        CSN_LOW();
         // flush the failed packet (it stays in the Tx FIFO; we could try to resend it by setting CE high)
         SPI_Write_Byte( FLUSH_TX );
         // resynch SPI
-        RADIO_CSN_HIGH();
+        CSN_HIGH();
 
     	transmit_lock = 0;
-    	_reset_pipe0_address();
-		_set_rx_mode();
+    	reset_pipe0_address();
+		set_rx_mode();
     	// indicate in the history that a packet was dropped by appending a 0.
     	tx_history <<= 1;
 
@@ -485,8 +493,8 @@ ISR(INT4_vect)
 
     // clear the interrupt flags.
 	status = _BV(RX_DR) | _BV(TX_DS) | _BV(MAX_RT);
-	_set_register(STATUS, &status, 1);
+	set_register(STATUS, &status, 1);
 
-    RADIO_CE_HIGH();
+    CE_HIGH();
 }
 
